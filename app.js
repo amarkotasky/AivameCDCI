208450f302cd1f087e738a15aa7e6b274a385011
amarkotasky <amarkotasky@gamil.com>
2018-07-03 14:41:55 +0530

    updated

--- app.js
+++ app.js
@@ -2,5 +2,5 @@ const express = require("express");
 var version='1.0.0'
 const app =express();
 console.log(version)
-app.get('/',(req,res)=>res.send('<h1 align="center">Welcome To Zivame</h1> Version: '+version));
+app.get('/',(req,res)=>res.send('<h1 align="center">Welcome To Zivame</h1> Version: '+version +' \n\r SecretToken: '+process.env.secret_token));
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
