b55aa41ffa10041166c2168ab07216efbb7872a4
amarkotasky <amarkotasky@gmail.com>
2018-07-04 11:06:09 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,5 +2,5 @@ const express = require("express");
 var version='1.0.0'
 const app =express();
 console.log(version)
-app.get('/',(req,res)=>res.send('<h1 align="center">Welcome To Zivame</h1> Version: '+version +' \n\nRunning-- Port: '+process.env.appPort));
+app.get('/',(req,res)=>res.send('<h1 align="center">Welcome To Zivame</h1> Version: '+version +' \n\nUpdated-- Port: '+process.env.appPort));
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
